54a8531fe19f0818175c4323adbccfb29d1dae61
邱梁 <qiuliang@sohochina.com>
2016-06-03 10:01:51 +0800

    change domain

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+categories=${1}
+
+cp -rf ./_site/index.html ../qiuliang.github.io/
+cp -rf ./_site/archives.html ../qiuliang.github.io/
+cp -rf ./_site/images ../qiuliang.github.io/
+
+cp -rf ./_site/page2 ../qiuliang.github.io/
+cp -rf ./_site/page3 ../qiuliang.github.io/
+#cp -rf ./_site/page4 ../qiuliang.github.io/
+#cp -rf ./_site/page5 ../qiuliang.github.io/
+
+cp -rf ./_site/$categories ../qiuliang.github.io/
+
+echo success
