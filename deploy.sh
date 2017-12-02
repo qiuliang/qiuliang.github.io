db4e9253a81f5c9e67df5dcd4a22a5e2e56cc3e1
邱梁 <qiuliang@sohochina.com>
2017-12-02 22:03:47 +0800

    topic

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ cp -rf ./_site/images ../qiuliang.github.io/
 
 cp -rf ./_site/page2 ../qiuliang.github.io/
 cp -rf ./_site/page3 ../qiuliang.github.io/
-#cp -rf ./_site/page4 ../qiuliang.github.io/
-#cp -rf ./_site/page5 ../qiuliang.github.io/
+cp -rf ./_site/page4 ../qiuliang.github.io/
+cp -rf ./_site/page5 ../qiuliang.github.io/
 
 cp -rf ./_site/$categories ../qiuliang.github.io/
 
